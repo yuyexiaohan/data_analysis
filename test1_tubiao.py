67521f43074fdc0bb3131923246aa000dee77af7
achjiang <htxz_jiang@163.com>
2018-11-20 19:35:39 +0800

    add first

---
+++ test1_tubiao.py
@@ -0,0 +1,16 @@
+# coding=utf-8 
+# Time : 2018/11/20 19:23 
+# Author : achjiang
+# File : test1_tubiao.py
+
+from matplotlib import pyplot as plt
+
+
+x = range(2, 26, 2)
+y = [15,13,14,5,17,20,33,34,35,36,29,20]
+
+# 绘图
+plt.plot(x, y)
+
+# 展示图形
+plt.show()
